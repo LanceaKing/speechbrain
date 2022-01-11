86610480337de2c46ae1a13ce4b3106bf0273432
LanceaKing <31365292+LanceaKing@users.noreply.github.com>
2022-01-11 10:37:20 +0800

    Update speaker_verification_cosine.py

--- recipes/CNCeleb/SpeakerRec/speaker_verification_cosine.py
+++ recipes/CNCeleb/SpeakerRec/speaker_verification_cosine.py
@@ -20,7 +20,6 @@ import speechbrain as sb
 from tqdm.contrib import tqdm
 from hyperpyyaml import load_hyperpyyaml
 from speechbrain.utils.metric_stats import EER, minDCF
-from speechbrain.utils.data_utils import download_file
 from speechbrain.utils.distributed import run_on_main
 
 
